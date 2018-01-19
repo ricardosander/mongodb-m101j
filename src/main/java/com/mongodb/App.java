a65f92b7356bd2c8bff9d14eca38ec7b8cf5a47a
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-01-19 01:03:31 -0200

    Using freemarker along side with Spark.

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,12 +1,44 @@
 package com.mongodb;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
 
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
-        Spark.get("/", (request, response) -> "Hello Spark!!!");
+
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(App.class, "/");
+
+        Spark.get("/", (request, response) -> {
+
+            StringWriter writer = new StringWriter();
+
+            try {
+
+                Template template = configuration.getTemplate("hello.ftl");
+
+                HashMap<String, Object> helloMap = new HashMap<>();
+                helloMap.put("name", "Frermarker");
+
+                template.process(helloMap, writer);
+            } catch (IOException | TemplateException e) {
+                Spark.halt(500);
+                e.printStackTrace();
+            }
+
+            return writer;
+        });
+
     }
+
 }
